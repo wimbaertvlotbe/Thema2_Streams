cf6c672e46548d1363ce9c0932afe241388072e5
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-13 13:43:27 +0200

    Add project files.

---
+++ Thema2_Strreams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Thema2_Strreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Opdracht rond Streams!");
+
+            //Maak gebruik van een filestream en binarywriter om een reeks
+            //double-waarden weg te schrijven naar een bestand.
+            //De double-waarden kan je random laten genereren.
+
+            //Lees vervolgens de getallen terug in en toon ze op het scherm.
+            //vergelijk wat op het scherm staat met de inhoud van het bestand.
+            //Opgelet: bekijk het bestand met een hex-editor.
+        }
+    }
+}
